d11a2cbb8d60d6f969a96c7e76b8d6cf7f87b9d9
Martin Guevara <martin_91@live.com.ar>
2022-12-05 11:01:24 -0300

    Agregado de componente Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 function Footer() {
   return (
     <footer>
-        <div className='box'>Box 1</div>
-        <div className='box'>Box 2</div>
-        <div className='box'>Box 3</div>
+        <div className="box__container">
+            <div className="box">
+                <div>Logo</div>
+                <div>Lorem</div>
+                <div>Input correo</div>
+            </div>
+            <div className="box">
+                <h3>Links</h3>
+                <div>Home</div>
+                <div>Sobre nosotros</div>
+                <div>Nuestros servicios</div>
+                <div>Proyectos</div>
+            </div>
+            <div className="box">
+                <h3>Nuestra compañía</h3>
+                <div>Dirección</div>
+            </div>
+        </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
