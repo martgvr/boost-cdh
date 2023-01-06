584890077bff2716faed0427b0e4d827005fa4af
Martin Guevara <martin_91@live.com.ar>
2023-01-06 15:23:59 -0300

    Desarrollo del proyecto

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import './welcome.css'
 import NavBar from "../NavBar/NavBar";
 
 function Welcome() {
   return (
     <>
       <NavBar />
-      <div>Welcome</div>
+      <div className="welcome">
+        <img src="./cloud2.png" alt="" className="cloud2"/>
+        <img src="./cloud1.png" alt="" className="cloud1"/>
+        <img src="./rocket.png" alt="" className="rocket"/>
+        <p className="text1">Despegamos con tus mejores ideas y aterrizamos haciéndolas realidad.</p>
+      </div>
     </>
   );
 }
