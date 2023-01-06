572db1cbf706c0a2f30ba47030f5df8f6f118871
Martin Guevara <martin_91@live.com.ar>
2023-01-06 19:57:30 -0300

    Desarrollo del proyecto

--- src/components/Subheader/Subheader.jsx
+++ src/components/Subheader/Subheader.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SubheaderCard from '../SubheaderCard/SubheaderCard'
 import './subheader.css'
 
 function Subheader() {
   return (
-    <div className='subheader'>Subheader</div>
+    <div className='subheader'>
+      <SubheaderCard title={'Desarrollo Web'} />
+      <SubheaderCard title={'Branding'} />
+      <SubheaderCard title={'Redes sociales'} />
+    </div>
   )
 }
 
