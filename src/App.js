0bdcbc5132ff68283f8bd21f3576bb69431ad958
Martin Guevara <martin_91@live.com.ar>
2023-01-06 22:09:30 -0300

    Desarrollo del proyecto

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Prices from './components/Prices/Prices';
 import Welcome from './components/Welcome/Welcome';
+import Who from './components/Who/Who';
 
 function App() {
   return (
     <>
       <Welcome />
       <About />
+      <Who />
       <Prices />
       <Contact />
       <Footer />
