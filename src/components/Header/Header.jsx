f94c87f1b90eae34dda1acee8967d6664955d284
Martin Guevara <martin_91@live.com.ar>
2023-01-07 14:17:03 -0300

    Desarrollo del proyecto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,18 @@ function Header() {
         <img src="./cloud1.png" alt="" className="cloud1" />
         <img src="./rocket.png" alt="" className="rocket" />
         <img src="./moon.png" alt="" className="moon" />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '90px', top: '55px', width: '7px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '5%', top: '107px', width: '2px', height: '3px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '69px', top: '151px', width: '4px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '331px', top: '77px', width: '11.83px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '10.3%', top: '239px', width: '11.83px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '47.5%', top: '60px', width: '5px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '44.8%', top: '187px', width: '4px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '50.6%', top: '439px', width: '11.83px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '75%', top: '100px', width: '7px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '83%', top: '50px', width: '11.83px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '91%', top: '120px', width: '3px' }} />
+        <img src="./circle.png" alt="" className="circle" style={{ left: '89%', top: '290px', width: '7px' }} />
       </div>
       
       <div className="text__container">
