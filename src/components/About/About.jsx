2bdc40e88afceb3418053a17fb41e93e80a0f798
Martin Guevara <martin_91@live.com.ar>
2023-01-07 14:27:27 -0300

    Desarrollo del proyecto

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,8 +11,18 @@ function About() {
         <p>Nuestro equipo empatiza con tu necesidad directa y crea el ambiente perfecto para que el resultado final de nuestro trabajo refleje la esencia de tu empresa.</p>
       </div>
       <div className='about__cards'>
-        <AboutCard title={'Misión'} image={'mision.png'} backgroundColor={'#75BFB2'} text={'Facilitar el despegue de las marcas y establecerlas en el mundo online con una estrategia clara para cada una de ellas.'} />
-        <AboutCard title={'Visión'} image={'vision.png'} backgroundColor={'#252526'} text={'Ser reconocida como la empresa que brinda un servicio INTEGRAL Y DE CALIDAD PREMIUM a una cartera distinguida de clientes a través de soluciónes que están a la vanguardia de las necesidades tecnológicas a nivel internacional.'} />
+        <AboutCard 
+          title={'Misión'} 
+          image={'mision.png'} 
+          backgroundColor={'#75BFB2'} 
+          text={'Facilitar el despegue de las marcas y establecerlas en el mundo online con una estrategia clara para cada una de ellas.'} 
+        />
+        <AboutCard 
+          title={'Visión'} 
+          image={'vision.png'} 
+          backgroundColor={'#252526'} 
+          text={'Ser reconocida como la empresa que brinda un servicio INTEGRAL Y DE CALIDAD PREMIUM a una cartera distinguida de clientes a través de soluciónes que están a la vanguardia de las necesidades tecnológicas a nivel internacional.'} 
+        />
       </div>
     </div>
   )
