fc02beda2d7f40737fdf505421b19d24a5a29924
Martin Guevara <martin_91@live.com.ar>
2023-01-08 02:03:38 -0300

    Cambios en la estructura general

--- src/components/FeedbackCarrousel/FeedbackCarrousel.jsx
+++ src/components/FeedbackCarrousel/FeedbackCarrousel.jsx
@@ -12,9 +12,21 @@ function FeedbackCarrousel() {
         image={'author.png'} 
       />
       <FeedbackCard 
-        text={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} 
+        text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa tincidunt dui ut ornare lectus sit amet est placerat.'} 
         author={'Lionel Messi'} 
         location={'paris'} 
+        image={'author.png'} 
+      />
+      <FeedbackCard 
+        text={'Condimentum vitae sapien pellentesque habitant morbi tristique. In massa tempor nec feugiat nisl pretium fusce. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida.'} 
+        author={'Leandro Paredes'} 
+        location={'Paris Saint-Germain'} 
+        image={'author.png'} 
+      />
+      <FeedbackCard 
+        text={'Amet mauris commodo quis imperdiet massa tincidunt nunc. Porttitor rhoncus dolor purus non enim. A iaculis at erat pellentesque adipiscing commodo elit. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus.'} 
+        author={'Alexis Mac Allister'} 
+        location={'Brighton & Hove Albion'} 
         image={'author.png'} 
       />
     </div>
