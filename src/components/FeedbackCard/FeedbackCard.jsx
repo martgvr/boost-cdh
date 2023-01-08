c3fc6c416cca98b9f8dde750b4ac0a25add89231
Martin Guevara <martin_91@live.com.ar>
2023-01-08 01:10:07 -0300

    Cambios en la estructura general

--- src/components/FeedbackCard/FeedbackCard.jsx
+++ src/components/FeedbackCard/FeedbackCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './feedbackcard.css'
 
-function FeedbackCard() {
+function FeedbackCard({ text, author, location, image }) {
   return (
     <div className='feedbackcard'>
       <img src="quote.png" alt="" />
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+      <p>{text}</p>
       <div className='feedbackcard__author'>
-        <div className='feedbackcard__author--image'><img src="author1.png" alt="" /></div>
+        <div className='feedbackcard__author--image'><img src={image} alt="Imagen del autor" /></div>
         <div className='feedbackcard__author--text'>
-          <h4>Julián Alvarez</h4>
-          <p>MANCHESTER</p>
+          <h4>{author}</h4>
+          <p>{location}</p>
         </div>
       </div>
     </div>
--- src/components/FeedbackCarrousel/FeedbackCarrousel.jsx
+++ src/components/FeedbackCarrousel/FeedbackCarrousel.jsx
@@ -5,8 +5,18 @@ import './feedbackcarrousel.css'
 function FeedbackCarrousel() {
   return (
     <div className='feedbackcarrousel'>
-      <FeedbackCard />
-      <FeedbackCard />
+      <FeedbackCard 
+        text={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} 
+        author={'Julián Alvarez'} 
+        location={'manchester'} 
+        image={'author.png'} 
+      />
+      <FeedbackCard 
+        text={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} 
+        author={'Lionel Messi'} 
+        location={'paris'} 
+        image={'author.png'} 
+      />
     </div>
   )
 }
