535dcab2f23f16160ca756f4c4c9fa1e03b50a30
Martin Guevara <martin_91@live.com.ar>
2023-01-10 01:40:05 -0300

    Desarrollo del proyecto

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -6,7 +6,9 @@ function Works() {
   return (
     <div className='works'>
         <h2>Un poco de nuestros trabajos realizados...</h2>
+        <img src="elipse.png" alt="" />
         <WorksCarrousel />
+        <img src="elipse.png" alt="" />
     </div>
   )
 }
